804480773d28d149d8f62b1c7bf2178acc6d2aa7
Alexey Prisukhin <wolasder@gmail.com>
2023-01-07 16:48:19 +0300

    написал программу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритмами.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+string[] array = new string[] { "1", "12", "123", "1234", "1" };
+
+void PrintArray(string[] array)
+{
+
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.Write($"{array[i]} ");
+	}
+	Console.WriteLine();
+}
+
+void Diffarray(string[] array)
+{
+		for (int i = 0; i < array.Length; i++)
+	{
+		string element = array[i];
+
+		if (element.Length <= 3)
+		{
+			array[i] = element;
+		}
+		else array[i] = string.Empty;
+	}
+}
+
+PrintArray(array);
+Diffarray(array);
+PrintArray(array);
